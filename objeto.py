7e7d1256981e23e6b08366d45c0c49ff453c6653
mystefy <162495468+juanitogarcia13215@users.noreply.github.com>
2025-09-29 21:13:42 -0500

    cambio
    cambio 2

---
+++ objeto.py
@@ -0,0 +1,13 @@
+from bosque import bosque
+
+def objeto ():
+    print("elige una opción  " \
+    "1: espada magica " \
+    "2: escudo encantado " \
+    "3: posión curativa "
+    "4: salir ")
+
+    objeto = input ("elije un objeto 1/2/3/4")
+
+    if objeto == "1":
+        print("espada de energia, eficaz siempre en el campo de batalla")
